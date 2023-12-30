09596b40dc9137b70b66dc7f29b763df68b1d2d7
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2023-12-31 04:04:44 +0800

    Compromiso inicial

---
+++ src/test/java/com/ramjava/java/diseno/patrones/buffer/JavaDisenoPatronesBufferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramjava.java.diseno.patrones.buffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDisenoPatronesBufferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
